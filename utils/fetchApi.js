8a9d2cb70267c06732956ae1c7c2c290eae11d40
Jahid Eljah <jahideljah@gmail.com>
2022-12-02 20:10:37 +0100

    hide it

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,7 +5,7 @@ export const baseUrl = "https://bayut.p.rapidapi.com";
 export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
-      "X-RapidAPI-Key": "b7ffeff074msh251af989fe30404p17988bjsn9ef19d0c8a77",
+      "X-RapidAPI-Key": process.env.REACT_APP_STATE_RAPID_API_KEY,
       "X-RapidAPI-Host": "bayut.p.rapidapi.com",
     },
   });
