293228ec00d6a44503678a7122d41af5d319cca8
Jahid Eljah <jahideljah@gmail.com>
2022-12-09 20:25:16 +0100

    Footer add

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -16,7 +16,7 @@ import { FiKey } from "react-icons/fi";
 const Navbar = () => (
   <Flex p="2" borderBottom="1px" borderColor="gray.100">
     <Box fontSize="3xl" color="blue.400" fontWeight="bold">
-      <Link href="/" paddingLeft="2">
+      <Link href="/" paddingleft="2">
         Realtor
       </Link>
     </Box>
---
+++ pages/Search.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Image from "next/legacy/image";
+import { Flex, Box, Text, Icon } from "@chakra-ui/react";
+import { BsFilter } from "react-icons/bs";
+import Search from "./Search";
+import Property from "./../components/Property";
+
+const Search = () => {
+  const [searchFilters, setSearchFilters] = useState(false);
+  const router = useRouter();
+
+  return (
+    <Box>
+      <Flex cursor="pointer" bg="gray.100"></Flex>
+      <Text>Search Property By Filters</Text>
+    </Box>
+  );
+};
+
+export default Search;
