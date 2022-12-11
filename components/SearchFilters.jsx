a27b1369758d1b4baa2a5120347d4e90a6d2cc06
Jahid Eljah <jahideljah@gmail.com>
2022-12-11 16:54:08 +0100

    searchFilter

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import {
+  Flex,
+  Select,
+  Box,
+  Text,
+  Input,
+  Spinner,
+  Icon,
+  Button,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { MdCacel } from "react-icons/md";
+import Image from "next/legacy/image";
+
+const SearchFilters = () => {
+  return <div>searchFilters</div>;
+};
+
+export default SearchFilters;
--- pages/Search.js
+++ pages/Search.js
@@ -2,18 +2,37 @@ import { useState } from "react";
 import { useRouter } from "next/router";
 import Image from "next/legacy/image";
 import { Flex, Box, Text, Icon } from "@chakra-ui/react";
-import { BsFilter } from "react-icons/bs";
 
-import Property from "../components/Property";
+import { BsFilter } from "react-icons/bs";
+import SearchFilters from "./../components/SearchFilters";
 
 const Search = () => {
   const [searchFilters, setSearchFilters] = useState(false);
   const router = useRouter();
 
+  /*  const searchFiltersHandler = () => {
+    setSearchFilters((prevFilters) => !prevFilters);
+  }; */
+
   return (
     <Box>
-      <Flex cursor="pointer" bg="gray.100"></Flex>
-      <Text>Search Property By Filters</Text>
+      <Flex
+        onClick={() => setSearchFilters((prevFilters) => !prevFilters)}
+        cursor="pointer"
+        bg="gray.100"
+        borderBottom="1px"
+        borderColor="gray.200"
+        p="2"
+        fontWeight="black"
+        fontSize="lg"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Text>Search Property By Filters &nbsp;</Text>
+        <Icon as={BsFilter} /* paddingLeft="1" */ />
+      </Flex>
+      {searchFilters && <SearchFilters />}
+      <Text fontSize="2xl" p="4" fontWeight="bold"></Text>
     </Box>
   );
 };
