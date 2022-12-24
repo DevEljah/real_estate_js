ddc6b3bf9c0e3355767f3c35408d548faa4a4afd
Jahid Eljah <jahideljah@gmail.com>
2022-12-24 22:11:05 +0100

    srerchFilter UI add

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -8,14 +8,39 @@ import {
   Spinner,
   Icon,
   Button,
+  filter,
 } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { MdCacel } from "react-icons/md";
 import Image from "next/legacy/image";
+import { filterData, getFilterValues } from "../utils/filterData";
 
 const SearchFilters = () => {
+  const [filters, setFilters] = useState(filterData);
+
+  /* const searchProperties = (filterValues) => {}; */
+
   return (
-    <Flex bg="gray.100" p="4" justfiyContent="center" flexWrap="wrap"></Flex>
+    <Flex bg="gray.100" p="4" justfiyContent="center" flexWrap="wrap">
+      {filters.map((filter) => (
+        <Box key={filter.queryName}>
+          <Select
+            /*  onChange={(e) =>
+              searchProperties({ [filter.queryName]: e.target.value })
+            } */
+            placeholder={filter.placeholder}
+            w="fit-content"
+            p="2"
+          >
+            {filter?.items?.map((item) => (
+              <option value={item.value} key={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+    </Flex>
   );
 };
 
