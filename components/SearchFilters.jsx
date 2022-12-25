d27f53027cd3e5cae9781a3c43a6dace4be673c0
Jahid Eljah <jahideljah@gmail.com>
2022-12-25 16:07:33 +0100

    filter function add

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -17,17 +17,28 @@ import { filterData, getFilterValues } from "../utils/filterData";
 
 const SearchFilters = () => {
   const [filters, setFilters] = useState(filterData);
+  const router = useRouter();
 
-  /* const searchProperties = (filterValues) => {}; */
+  const searchProperties = (filterValues) => {
+    const path = router.pathname;
+    const { query } = router;
+
+    const values = getFilterValues(filterValues);
+    values.forEach((item) => {
+      query[item.name] = item.value; // basically just updating the query
+    });
+
+    router.push({ pathname: path, query });
+  };
 
   return (
     <Flex bg="gray.100" p="4" justfiyContent="center" flexWrap="wrap">
       {filters.map((filter) => (
         <Box key={filter.queryName}>
           <Select
-            /*  onChange={(e) =>
+            onChange={(e) =>
               searchProperties({ [filter.queryName]: e.target.value })
-            } */
+            }
             placeholder={filter.placeholder}
             w="fit-content"
             p="2"
